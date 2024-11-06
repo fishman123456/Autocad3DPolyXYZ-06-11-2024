844b53985d539109da3301a716b095c9c9d7f350
fishman123456 <den9152@yandex.ru>
2024-11-06 22:23:54 +0700

    Initial commit

---
+++ Class1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Net.Mime.MediaTypeNames;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.Geometry;
+using Application = Autodesk.AutoCAD.ApplicationServices.Application;
+
+
+public class Class1
+{
+public void Draw3DPolyline(Point3dCollection points)
+{
+    Database db = Application.DocumentManager.MdiActiveDocument.Database;
+    using (Transaction tr = db.TransactionManager.StartTransaction())
+    {
+        BlockTable bt = tr.GetObject(db.BlockTableId, OpenMode.ForRead) as BlockTable;
+        BlockTableRecord btr = tr.GetObject(bt[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
+
+        Polyline3d poly = new Polyline3d();
+
+        for (int i = 0; i < points.Count; i++)
+        {
+            Point3d currentPoint = points[i];
+            Point3d nearestPoint = FindNearestPoint(currentPoint, points);
+
+                //poly.AppendVertex(i, currentPoint, 0, 0, 0);
+                
+                //poly.AppendVertex(currentPoint);
+        }
+
+        btr.AppendEntity(poly);
+        tr.AddNewlyCreatedDBObject(poly, true);
+        tr.Commit();
+    }
+}
+
+public Point3d FindNearestPoint(Point3d currentPoint, Point3dCollection points)
+{
+    double minDistance = double.MaxValue;
+    Point3d nearestPoint = new Point3d();
+
+    foreach (Point3d point in points)
+    {
+        if (point != currentPoint)
+        {
+            double distance = currentPoint.DistanceTo(point);
+            if (distance < minDistance)
+            {
+                minDistance = distance;
+                nearestPoint = point;
+            }
+        }
+    }
+
+    return nearestPoint;
+}
+
+}
